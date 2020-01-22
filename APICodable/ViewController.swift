0c94ebf103cb03de30d1409396b4f9e4efa723e1
Mac <sudiptoroy@sudipto.local>
2020-01-22 16:00:15 +0600

    Show api data with URL Session

--- APICodable/ViewController.swift
+++ APICodable/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Alamofire
+
+struct Country: Decodable {
+    let name: String
+    let capital: String
+    let region: String
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
+        
+        let url = "https://restcountries.eu/rest/v2/all"
+        let urlObj = URL(string: url)
+        
+        URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
+            
+            do{
+                let countries = try JSONDecoder().decode([Country].self, from: data!)
+                
+                for Country in countries {
+                    print(Country.name + ":" + Country.capital)
+                }
+                
+            } catch{
+                print("Error")
+            }
+        }.resume()
     }
-
-
 }
 
